c92f812b3f2bb2870cfdea3b0b053c43496acfb5
RitikOplify <ritik.ranjan@oplify.in>
2025-03-11 14:06:00 +0530

    update2

--- app/collection/[id]/page.js
+++ app/collection/[id]/page.js
@@ -5,8 +5,7 @@ import React, { use } from "react";
 function page({ params }) {
   const { id } = use(params);
   const product = collections.find((product) => product.id === id);
-  const collection = collections.filter((product) => product.id !== id);
-  return <RolexProductCard product={product} collections={collection} />;
+  return <RolexProductCard product={product} />;
 }
 
 export default page;
